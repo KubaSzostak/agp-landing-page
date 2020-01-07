0965b65fd8d84f17eb1bcaf8a1541e84480a4aa7
kubaszostak <kuba.szostak@gmail.com>
2020-01-07 12:35:05 +0100

    fix agpLandingPageConfig

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var agpLandingPageConfig = {
+const agpLandingPageConfig = {
     portalUrl: "", // "https://mapy.umgdy.gov.pl/portal",  // "https://www.arcgis.com",            
     group: ""      // "9227744bd89342429da120fb3bba224a"   // "1d1f24e8556642f49448f1c88b5a571b" 
 };
